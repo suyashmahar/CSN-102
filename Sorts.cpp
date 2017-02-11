49ba7fdf65b9f6c51879e96bc1cab0640cc5c9d9
suyashmahar <suyash12mah.uec2016@iitr.ac.in>
2017-02-11 05:47:46 +0530

    Add Selection Sort to Sorts.cpp

--- Sorts.cpp
+++ Sorts.cpp
@@ -1,11 +1,14 @@
 #include<cstdlib>
 #include<iostream>
 #include<string>
+#include<math.h>
 
 using namespace std;
 
 void printArr(int* a, int length);
 int* bubbleSort(int* input, int k);
+int* selectionSort(int* input, int k);
+int* bucketSort(int* input, int k);
 
 int ARR_LEN = 100;
 
@@ -21,6 +24,14 @@ int main(){
     printArr(array, ARR_LEN);
     cout << endl << "Sorting array using bubbleSort..." << endl;
     printArr(bubbleSort(array, ARR_LEN), ARR_LEN);
+    
+    for (int i = 0; i < ARR_LEN; i++){
+        array[i] = i;
+        //array[i] = rand() % 1000 + 1;
+    }
+
+    cout << endl << "Sorting array using selectionSort..." << endl;
+    printArr(selectionSort(array, ARR_LEN), ARR_LEN);
     cout  << endl << "exiting..." << endl;
 }
 
@@ -41,6 +52,45 @@ int* bubbleSort(int* input, int k) {
                 input[j+1] ^= input[j];
                 input[j] ^= input[j+1];
             }
+        }
+    }
+    return input;
+}
+
+int* selectionSort(int* input, int k){
+    int* temp = input;
+    for (int i = 0; i < k; i++){
+        for (int j = i; j < k; j++){
+            temp = input[j] <= *temp ? &input[j] : temp; 
+        }
+
+        if (input[i] < *temp){
+            input[i] ^= *temp;
+            *temp ^= input[i];
+            input[i] ^= *temp;   
+
+            *temp = input[i+1];     
+        }
+    }
+    return input;
+}
+
+int* bucketSort(int* input, int k){
+    int max = *input;
+    for (int i = 0; i < k; i++){
+        max = max > input[i] ? max : input[i];
+    }
+    int* bucketArr = new int[max];
+    
+    for (int i = 0; i < k; i++){
+        bucketArr[i]++;
+    }
+
+    int* result = new int[k];
+
+    for (int i = 0; i < max; i++){
+        if (bucketArr[i]>0){
+            
         }
     }
     return input;
